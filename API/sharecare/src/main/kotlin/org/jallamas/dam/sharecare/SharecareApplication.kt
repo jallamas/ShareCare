70da9e22719de5336a95ef1252f3ca7ab6932784
José Antonio Llamas Álvarez <jallamasalvarez@gmail.com>
2020-05-22 13:22:21 +0200

    Initial commit

---
+++ API/sharecare/src/main/kotlin/org/jallamas/dam/sharecare/SharecareApplication.kt
@@ -0,0 +1,11 @@
+package org.jallamas.dam.sharecare
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SharecareApplication
+
+fun main(args: Array<String>) {
+	runApplication<SharecareApplication>(*args)
+}
---
+++ API/sharecare/src/test/kotlin/org/jallamas/dam/sharecare/SharecareApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.jallamas.dam.sharecare
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SharecareApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
